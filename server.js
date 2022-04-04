72df81c0037f8bc794d7c5af69f9f8eceedfcb0a
MacBook Pro <macbookpro@Tanyos-macbookpro.local>
2022-04-04 19:11:58 +0300

    starting point html name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/heroku-deploy'));
+app.use(express.static(__dirname + '/dist/softuni-frontend'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
+res.sendFile(path.join(__dirname+'/dist/softuni-frontend/index.html'));
 });
 
 app.listen(4200);
