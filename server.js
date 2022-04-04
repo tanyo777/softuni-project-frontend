3aec4a677c3b81ad8922c90b50a5d61b84ff468c
MacBook Pro <macbookpro@Tanyos-macbookpro.local>
2022-04-04 19:17:47 +0300

    build folder path

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/softuni-frontend'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/softuni-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
 
 app.listen(4200);
